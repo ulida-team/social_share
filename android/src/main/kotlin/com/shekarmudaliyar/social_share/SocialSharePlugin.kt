b805166057c411dcc89b0d4230d2c435d7621ec3
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2026-08-09 22:29:18 +0900

    Remove unused import and update misc.xml

--- android/src/main/kotlin/com/shekarmudaliyar/social_share/SocialSharePlugin.kt
+++ android/src/main/kotlin/com/shekarmudaliyar/social_share/SocialSharePlugin.kt
@@ -16,7 +16,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.net.URLEncoder
 import android.content.ClipData
